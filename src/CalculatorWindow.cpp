dc9f967a92142800803e2bffd348f2cc0c34c2ca
Jakub Wysocki <kuba.w108@gmail.com>
2018-01-25 08:33:31 +0100

    Add event handling

--- src/CalculatorWindow.cpp
+++ src/CalculatorWindow.cpp
@@ -9,11 +9,28 @@
 
 #include "CalculatorWindow.hpp"
 #include "ui_CalculatorWindow.h"
+#include <cassert>
 
 CalculatorWindow::CalculatorWindow( QWidget* parent )
     : QMainWindow( parent ), ui( new Ui::CalculatorWindow )
 {
     ui->setupUi( this );
 }
+
+void CalculatorWindow::on_inputNum0_clicked() { assert( 0 ); }
+void CalculatorWindow::on_inputNum1_clicked() {}
+void CalculatorWindow::on_inputNum2_clicked() {}
+void CalculatorWindow::on_inputNum3_clicked() {}
+void CalculatorWindow::on_inputNum4_clicked() {}
+void CalculatorWindow::on_inputNum5_clicked() {}
+void CalculatorWindow::on_inputNum6_clicked() {}
+void CalculatorWindow::on_inputNum7_clicked() {}
+void CalculatorWindow::on_inputNum8_clicked() {}
+void CalculatorWindow::on_inputNum9_clicked() {}
+
+void CalculatorWindow::on_inputOperDivis_clicked() {}
+void CalculatorWindow::on_inputOperMinus_clicked() {}
+void CalculatorWindow::on_inputOperMulti_clicked() {}
+void CalculatorWindow::on_inputOperPlus_clicked() {}
 
 CalculatorWindow::~CalculatorWindow() { delete ui; }
--- src/CalculatorWindow.hpp
+++ src/CalculatorWindow.hpp
@@ -24,6 +24,23 @@ public:
     explicit CalculatorWindow( QWidget* parent = 0 );
     ~CalculatorWindow();
 
+private slots:
+    void on_inputNum0_clicked();
+    void on_inputNum1_clicked();
+    void on_inputNum2_clicked();
+    void on_inputNum3_clicked();
+    void on_inputNum4_clicked();
+    void on_inputNum5_clicked();
+    void on_inputNum6_clicked();
+    void on_inputNum7_clicked();
+    void on_inputNum8_clicked();
+    void on_inputNum9_clicked();
+
+    void on_inputOperDivis_clicked();
+    void on_inputOperMinus_clicked();
+    void on_inputOperMulti_clicked();
+    void on_inputOperPlus_clicked();
+
 private:
     Ui::CalculatorWindow* ui;
 };
